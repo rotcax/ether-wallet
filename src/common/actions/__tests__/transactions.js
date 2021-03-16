0e98f473f193f7cb10612b2f347372f0ed0c6201
Victor Caceres <caceres.vc95@gmail.com>
2021-03-16 16:40:24 -0400

    add jest

---
+++ src/common/actions/__tests__/transactions.js
@@ -0,0 +1,13 @@
+import { wallet as WalletStore } from '../../stores'
+import { Wallet as WalletUtils, Transaction as TransactionUtil } from '../../utils'
+import * as Transactions from '../transactions'
+import ethers from 'ethers'
+
+const WALLET_PK = '62384683889eae6de8440eb735856f31bb4f17815888f847c8567b3c87f00be8'
+const DESTINATION_ADDRESS = '0x407428BF09ea7Dac2824A64AfE88171041a02b14'
+
+describe('TransactionsActions', () => {
+  beforeEach(() => WalletStore.reset())
+
+
+})
